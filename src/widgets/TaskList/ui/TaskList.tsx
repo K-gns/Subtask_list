4f1a721d19e413b35084cdd8b6de8fa867807a1a
Filatov Dmitry <Filtv.Dmitry@gmail.com>
2024-08-14 23:11:47 +0500

    Added click task to toggle subtasks. Added dropdown icon. ToDo: , add/delete/change task features

--- src/widgets/TaskList/ui/TaskList.tsx
+++ src/widgets/TaskList/ui/TaskList.tsx
@@ -1,5 +1,8 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './TaskList.module.scss'
+import React from "react";
+import DropdownIconCollapsed from 'shared/assets/icons/dropdown_icon_collapsed_16x16.svg'
+import DropdownIconExpanded from 'shared/assets/icons/dropdown_icon_expanded_16x16.svg'
 
 interface TaskListProps {
     className?: string;
@@ -17,46 +20,50 @@ const taskData = [
     },
     {label: "Task 3",
         subtask: [
-            {label: "Sub task 1"},
-            {label: "Sub task 2"},
-            {label: "Sub task 3"},
+            {label: "Subtask 1"},
+            {label: "Subtask 2"},
             {
-                label: "Sub task 3",
-                subtask: [{label: "Sub sub task 1"}, {label: "Sub sub task 2"}]
+                label: "Subtask 3",
+                subtask: [{label: "Subsubtask 1"}, {label: "Subsubtask 2"}]
             }
         ]
     },
     {label: "Task 4",
         subtask: [{label: "Subtask 1"}, {label: "Subtask 2"}]
     },
+    {label: "Task 5"},
 ]
 
 export const TaskList = ({className}: TaskListProps) => {
 
-    const toggleSubtask = () => {
-       console.log("clicked")
+    const toggleSubtask = (e : React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+        e.stopPropagation()
+        let subtask = (e.target as HTMLElement).querySelector("ul")
+
+        if (!subtask) return
+        subtask.style.display = subtask.style.display  !== "block" ? "block" : "none";
     }
 
     const renderSubtask = (subtask : Task[]) => {
-        console.log("subtask is", subtask)
-        subtask.map((item) => {
-            console.log("item is", item)
-        })
-        return <ul>
+
+        return (
+        <ul className={classNames("", {}, [cls.subtask])}>
             {subtask.map((item, index) =>
-                <li key={index} onClick={toggleSubtask}>
+                <li key={index} onClick={toggleSubtask} className={item.subtask ? cls.haveSubtasks : cls.noSubtasks}>
+                    {item.subtask && <span className={cls.dropdownIcon}><DropdownIconCollapsed /></span>}
                     {item.label}
                     {item.subtask && renderSubtask(item.subtask)}
                 </li>
             )}
         </ul>
-    }
+    )}
 
     return (
         <div className={classNames(cls.TaskList, {}, [className])}>
             <ul>
                 {taskData.map((item, index) =>
-                    <li key={index} onClick={toggleSubtask}>
+                    <li key={index} onClick={toggleSubtask} className={item.subtask ? cls.haveSubtasks : cls.noSubtasks}>
+                        {item.subtask && <span className={cls.dropdownIcon}><DropdownIconCollapsed /></span>}
                         {item.label}
                         {item.subtask && renderSubtask(item.subtask)}
                     </li>
