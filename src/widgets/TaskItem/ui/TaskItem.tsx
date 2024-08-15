652d2cadffd50c825a9e72c57bd4f864b71c0afd
Filatov Dmitry <Filtv.Dmitry@gmail.com>
2024-08-15 21:22:31 +0500

    Changed structure - divided tasklist logic into TaskItems and Tasklist

--- src/widgets/TaskItem/ui/TaskItem.tsx
+++ src/widgets/TaskItem/ui/TaskItem.tsx
@@ -1,15 +1,41 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './TaskItem.module.scss'
-import { Task } from 'widgets/TaskList/ui/TaskList'
+import {Task} from 'widgets/TaskList/ui/TaskList'
 import DropdownIconCollapsed from "shared/assets/icons/dropdown_icon_collapsed_16x16.svg";
-import React from "react";
+import React, {useState} from "react";
+
 interface TaskProps {
     className?: string;
     task: Task;
-    toggleSubtask: (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => void
+    isInMainList: boolean;
+    hidden: boolean;
+    toggleSubtask?: (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => void
 }
 
-export const TaskItem = ({className, task, toggleSubtask}: TaskProps) => {
+export const TaskItem = ({className, task, isInMainList, hidden}: TaskProps) => {
+    const [childsHidden, setChildsHidden] = useState(true)
 
-    return
+    const expandChildrensComment = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+        e.stopPropagation()
+        setChildsHidden((state) => !state)
+    }
+
+    return (
+        <ul className={classNames("", {}, [hidden ? cls.hidden : ""])}>
+
+            {/*Рендер самого комментария*/}
+            <li key={task.id} onClick={expandChildrensComment}
+                className={task.subtasks ? cls.haveSubtasks : cls.noSubtasks}>
+                {task.subtasks && <span className={cls.dropdownIcon}><DropdownIconCollapsed/></span>}
+                {task.label}
+            </li>
+
+            {/*Рендер дочерних комментариев*/}
+            {task.subtasks && task.subtasks.map((item, index) =>
+                <TaskItem key={item.id} task={item} isInMainList={true} hidden={childsHidden}/>
+            )}
+        </ul>
+
+
+    )
 };
--- src/widgets/TaskList/ui/TaskList.tsx
+++ src/widgets/TaskList/ui/TaskList.tsx
@@ -12,81 +12,79 @@ interface TaskListProps {
 export type Task = {
     id: number;
     label: string;
-    subtask?: Task[];
+    subtasks?: Task[];
 }
 
-const taskData = [
-    {
-        id: 101,
-        label: "Task 1"
-    },
-    {
-        id: 102,
-        label: "Task 2",
-        subtask: [
-            {id: 103, label: "Subtask 1"},
-            {id: 104, label: "Subtask 2"}
-        ]
-    },
-    {
-        id: 105,
-        label: "Task 3",
-        subtask: [
-            {id: 106, label: "Subtask 1"},
-            {id: 107, label: "Subtask 2"},
-            {
-                id: 108,
-                label: "Subtask 3",
-                subtask: [
-                    {id: 109, label: "Subsubtask 1"},
-                    {id: 110, label: "Subsubtask 2"}
+const taskData = {
+    id: 1,
+    subtasks: [
+        {
+            id: 101,
+            label: "Task 1"
+        },
+        {
+            id: 102,
+            label:
+                "Task 2",
+            subtasks:
+                [
+                    {id: 103, label: "Subtask 1"},
+                    {id: 104, label: "Subtask 2"}
                 ]
-            }
-        ]
-    },
-    {
-        id: 111,
-        label: "Task 4",
-        subtask: [
-            {id: 112, label: "Subtask 1"},
-            {id: 113, label: "Subtask 2"}
-        ]
-    },
-    {
-        id: 114,
-        label: "Task 5"
-    },
-]
+        },
+        {
+            id: 105,
+            label:
+                "Task 3",
+            subtasks:
+                [
+                    {id: 106, label: "Subtask 1"},
+                    {id: 107, label: "Subtask 2"},
+                    {
+                        id: 108,
+                        label: "Subtask 3",
+                        subtasks: [
+                            {id: 109, label: "Subsubtask 1"},
+                            {id: 110, label: "Subsubtask 2"}
+                        ]
+                    }
+                ]
+        },
+        {
+            id: 111,
+            label:
+                "Task 4",
+            subtasks:
+                [
+                    {id: 112, label: "Subtask 1"},
+                    {id: 113, label: "Subtask 2"}
+                ]
+        },
+        {
+            id: 114,
+            label:
+                "Task 5"
+        },
+
+    ]
+}
 
 export const TaskList = ({className}: TaskListProps) => {
 
     const toggleSubtask = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
         e.stopPropagation()
-        let subtask = (e.target as HTMLElement).querySelector("ul")
+        let subtasks = (e.target as HTMLElement).querySelector("ul")
 
-        if (!subtask) return
-        subtask.style.display = subtask.style.display !== "block" ? "block" : "none";
+        if (!subtasks) return
+        subtasks.style.display = subtasks.style.display !== "block" ? "block" : "none";
     }
 
-    const renderSubtask = (subtask: Task[], isMainList: boolean) => {
-
-        return (
-            <ul className={classNames("", {}, [!isMainList ? cls.subtask : ""])}>
-                {subtask.map((item, index) =>
-                    <li key={item.id} onClick={toggleSubtask}
-                        className={item.subtask ? cls.haveSubtasks : cls.noSubtasks}>
-                        {item.subtask && <span className={cls.dropdownIcon}><DropdownIconCollapsed/></span>}
-                        {item.label}
-                        {item.subtask && renderSubtask(item.subtask, false)}
-                    </li>
-                )}
-            </ul>
-        )
-    }
 
     return (
         <div className={classNames(cls.TaskList, {}, [className])}>
-            {renderSubtask(taskData, true)}
+            {taskData.subtasks.map((item) =>
+                <TaskItem key={item.id} task={item} isInMainList={true} hidden={false}/>
+            )}
         </div>
     );
 };
